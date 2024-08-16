ce232c1f75d2cdb638bd41993f463b5502c23372
Vikas <7014.vikas@gmail.com>
2024-08-16 11:33:16 +0530

    project-sandbox

---
+++ src/01-e-signature/Esignature.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../components/Title';
+
+export default function Esignature() {
+  return (
+    <div className='container text-center'>
+
+        <Title />
+    </div>
+  )
+}
+
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Title() {
+  return 
+    <h1 className='title text-center'>Title</h1>;
+  
+}
